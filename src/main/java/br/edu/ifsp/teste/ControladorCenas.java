e12ccb7bf019905ea70ffc54ab89d0e799c7a589
FernandoDacanal <fernandodacanal10@gmail.com>
2025-02-04 22:01:57 -0300

    Corrigir Tela

--- src/main/java/br/edu/ifsp/teste/ControladorCenas.java
+++ src/main/java/br/edu/ifsp/teste/ControladorCenas.java
@@ -19,35 +19,13 @@ public class ControladorCenas
     private Scene scene;
     private Parent root;
 
-    public void novoGrupo(ActionEvent event) throws Exception {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("folder-create.fxml"));
-            Stage stage = new Stage();
-            stage.setTitle("Criar novo Grupo");
-            stage.setScene(new Scene(loader.load()));
-    
-            stage.setResizable(false);
-    
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public void irNota(ActionEvent event) throws IOException 
     {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("note-view.fxml"));
-            Stage stage = new Stage();
-            stage.setTitle("Nova nota");
-            stage.setScene(new Scene(loader.load()));
-    
-            stage.setResizable(false); 
-    
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Parent root = (Parent)FXMLLoader.load(this.getClass().getResource("note-view.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(root);
+        this.stage.setScene(this.scene);
+        this.stage.show();
     }
 
     public void irMenu(ActionEvent event) throws IOException 
@@ -58,31 +36,4 @@ public class ControladorCenas
         stage.setScene(scene);
         stage.show();
     }
-    @FXML
-    private TreeView<String> treeView;
-    /*
-    // This method loads files into the TreeView
-    public void initialize() {
-        File rootDirectory = new File("src/main/resources/notas"); // Specify your folder path here
-        TreeItem<String> rootItem = new TreeItem<>(rootDirectory.getName());
-        rootItem.setExpanded(true);  // Optionally expand the root item
-
-        loadFiles(rootDirectory, rootItem);
-
-        treeView.setRoot(rootItem);
-    }
-    // Recursively load files into the TreeView
-    private void loadFiles(File folder, TreeItem<String> parentItem) {
-        File[] files = folder.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                TreeItem<String> fileItem = new TreeItem<>(file.getName());
-                parentItem.getChildren().add(fileItem);
-                if (file.isDirectory()) {
-                    loadFiles(file, fileItem);  // Recurse if directory
-                }
-            }
-        }
-    }
-        */
 }
